5aa0ea2fe9bb6453fb4da0951ea41535510bf6f7
Jerson Reyes <jerson.reyes.a@bulsu.edu.ph>
2022-11-12 23:36:34 +0800

    A

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express()
+const port = 3000
+var fs = require('fs');
+
+app.get('/', (req, res) => {
+    fs.readFile('./db.txt', 'utf8', (error, data) => {
+        if(error){
+           console.log(error);
+           return;
+        }
+        res.send(data);
+   })
+})
+
+app.get('/update', (req, res) => {
+
+    fs.writeFile ("db.txt", String(req.query.subdomain), function(err) {
+        if (err) throw err;
+            res.send('complete');
+        }
+    );
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
